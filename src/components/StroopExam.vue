1ea467ac77192603d01534d8f4bf956f849b7449
David Torosyan <davidtorosyan.git@gmail.com>
2023-03-12 15:06:23 -0700

    Add result table

--- src/components/StroopExam.vue
+++ src/components/StroopExam.vue
@@ -9,8 +9,17 @@ const lang: Ref<Lang> = ref('en')
 const visualColor: Ref<Color> = ref('red')
 const lexicalColor: Ref<Color> = ref('red')
 
+interface GuessResult {
+  index: number
+  visual: Color
+  lexical: Color
+  guess: Color
+}
+
 const hex = computed(() => colors[visualColor.value].hex)
 const name = computed(() => colors[lexicalColor.value].names[lang.value])
+
+const guesses: Ref<GuessResult[]> = ref([])
 
 function random(min: number, max: number): number {
   return min + Math.floor(Math.random() * (max - min + 1));
@@ -40,6 +49,16 @@ const langOptions = ref(chosenLangs.map((choice) => ({
   value: choice
 })))
 
+function guess(color: Color) {
+  guesses.value.push({
+    index: guesses.value.length,
+    visual: visualColor.value,
+    lexical: lexicalColor.value,
+    guess: color
+  })
+  next()
+}
+
 </script>
 
 <template>
@@ -55,6 +74,26 @@ const langOptions = ref(chosenLangs.map((choice) => ({
         {{ option.text }}
       </option>
     </select>
+  </div>
+  <div class="controls">
+    <button @click="guess(choice)" :style="`background-color: ${colors[choice].hex};`" v-for="choice in chosenColors"
+      :key="choice">
+      {{ choice }}
+    </button>
+  </div>
+  <div class="guesses">
+    <table>
+      <tr>
+        <th>Visual Color</th>
+        <th>Written Color</th>
+        <th>Guess</th>
+      </tr>
+      <tr v-for="guess in guesses" :key="guess.index">
+        <td>{{ guess.visual }}</td>
+        <td>{{ guess.lexical }}</td>
+        <td>{{ guess.guess }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -73,5 +112,13 @@ const langOptions = ref(chosenLangs.map((choice) => ({
   background-color: beige;
   padding-top: 3rem;
   padding-bottom: 3rem;
+}
+
+.controls>button {
+  border: none;
+  min-width: 100px;
+  min-height: 100px;
+  font-size: 0;
+  margin: 10px;
 }
 </style>
