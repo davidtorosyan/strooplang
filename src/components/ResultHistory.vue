92e003abbd778dd166de063656499eced71d7859
David Torosyan <davidtorosyan.git@gmail.com>
2023-03-12 21:57:55 -0700

    Fix desktop shift

--- src/components/ResultHistory.vue
+++ src/components/ResultHistory.vue
@@ -8,13 +8,13 @@ import { computed, type ComputedRef } from 'vue';
 
 const examHistory = useExamHistoryStore()
 
-const examResults: ComputedRef<TestResultInfo[]> = computed(() => examHistory.results.map(r => ({
+const examResults: ComputedRef<TestResultInfo[]> = computed(() => examHistory.results.slice(0).reverse().map(r => ({
   ...r,
   stats: computeStats(r),
 })))
 
 function formatDate(date: Date): string {
-  return date.toLocaleDateString('en-us', { year: "numeric", month: "short", day: "numeric" })
+  return date.toLocaleDateString('en-us', { year: "numeric", month: "short", day: "numeric", hour: "numeric", minute: "numeric" })
 }
 
 </script>
@@ -23,8 +23,8 @@ function formatDate(date: Date): string {
   <h3>
     Results
   </h3>
-  <div>
-    <table v-if="examResults.length > 0" class="results">
+  <div class="results">
+    <table v-if="examResults.length > 0">
       <tr>
         <th>Date</th>
         <th>Language</th>
@@ -33,7 +33,7 @@ function formatDate(date: Date): string {
       <tr v-for="result in examResults" :key="result.index">
         <td>{{ formatDate(result.date) }}</td>
         <td>{{ langRecords[result.lang].name }}</td>
-        <td v-if="result.stats">{{ result.stats.deltaPercent * 100 }}%</td>
+        <td v-if="result.stats">{{ (result.stats.deltaPercent * 100).toFixed(0) }}%</td>
         <td v-else colspan="100%">Invalid responses!</td>
       </tr>
     </table>
@@ -42,6 +42,10 @@ function formatDate(date: Date): string {
 </template>
 
 <style scoped>
+.results {
+  height: 300px;
+}
+
 table,
 th,
 td {
