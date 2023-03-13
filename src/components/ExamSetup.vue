187362f530a5ac9a112828352cb34bc83ceee6ef
David Torosyan <davidtorosyan.git@gmail.com>
2023-03-12 22:20:33 -0700

    Disable language change

--- src/components/ExamSetup.vue
+++ src/components/ExamSetup.vue
@@ -3,8 +3,10 @@ import { ref } from 'vue'
 import { langSet } from '@/data/sets';
 import { useExamSettingsStore } from '@/stores/examSettings'
 import { langRecords } from '@/data/colors';
+import { useExamStateStore } from '@/stores/examState';
 
 const examSettings = useExamSettingsStore()
+const examState = useExamStateStore()
 
 const langOptions = ref(langSet.map((choice) => ({
   text: langRecords[choice].name,
@@ -18,7 +20,7 @@ const langOptions = ref(langSet.map((choice) => ({
     Setup
   </h3>
   <div>
-    <select v-model="examSettings.lang">
+    <select :disabled="examState.running" v-model="examSettings.lang">
       <option v-for="option in langOptions" :value="option.value" :key="option.value">
         {{ option.text }}
       </option>
